43db4e93f561dbe43fb41b84e3e6234d8fcd452c
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-13 17:05:51 +0100

    Set up base header

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import Header from './Header.js'
+import Navbar from './Navbar'
+import Reviews from './Reviews'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Reviews />
+    </>
+  );
+}
+
+export default App;
