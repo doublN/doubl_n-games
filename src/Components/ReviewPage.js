6be2979034061c7b185a5b36cc253b433c7a5932
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-14 15:39:11 +0100

    Set up ReviewPage skeleton and set up route

---
+++ src/Components/ReviewPage.js
@@ -0,0 +1,11 @@
+import Review from "./Review"
+import CommentSection from "./CommentSection"
+
+export default function ReviewPage() {
+  return (
+    <>
+        <Review />
+        <CommentSection />
+    </>
+  )
+}
