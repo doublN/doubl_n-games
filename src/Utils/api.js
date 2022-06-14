080cadf30b1e4628150e543e46b4d9a6f652f502
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-14 15:28:30 +0100

    Changed to use axio params

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -3,13 +3,13 @@ const axios = require('axios').default;
 const api = axios.create({baseURL : "https://doubl-n-games.herokuapp.com/api"});
 
 function getReviews (category){
-    let route = "/reviews";
+    let params = {}
 
     if(category !== undefined || category === 'Choose+a+category'){
-        route += `?category=${category}`
+        params.category = category;
     }
 
-    return api.get(route).then(({data : {reviews}}) =>{
+    return api.get("/reviews", {params}).then(({data : {reviews}}) =>{
         return reviews;
     })
 }
