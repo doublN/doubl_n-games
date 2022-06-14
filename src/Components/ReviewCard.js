ae0f255e93a6e6d81c2aa63a305276b591f9638c
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-14 12:04:40 +0100

    Implemented more styling for desktop

--- src/Components/ReviewCard.js
+++ src/Components/ReviewCard.js
@@ -5,14 +5,20 @@ export default function ReviewCard({review}) {
   
     return (
     <li className="reviewCard">
+    <div className="reviewTitle">
         <h3>{review.title}</h3>
+    </div>
+    <div className="reviewImg">
         <img src={review.review_img_url} alt={review.title}/>
+    </div>
+    <div className="reviewDetails">
         <p>Designer: {review.designer}</p>
         <p>Owner: {review.owner}</p>
         <p>Category: {review.category}</p>
         <p>{review.comment_count} comments</p>
         <p>{review.votes} votes</p>
         <p>Posted on: {date}</p>
+    </div>
     </li>
   )
 }
