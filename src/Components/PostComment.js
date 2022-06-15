e51414c4d15ac0bb4304dd83d0faba37619bbea0
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-15 16:09:34 +0100

    Set up base comment form

--- src/Components/PostComment.js
+++ src/Components/PostComment.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import {useState} from 'react'
 
 export default function PostComment() {
+  const [comment, setComment] = useState("")
+
+  function handleSubmit(event){
+      //api call
+  }
+
+  console.log(comment);
+
   return (
-    <div>Post a comment (placeholder)</div>
+    <form onSubmit={handleSubmit}>
+      <label>Post a Comment:</label>
+      <input type="text" onChange={(event) => {setComment(event.target.value)}} value={comment}/>
+      <input type="submit"></input>
+    </form>
   )
 }
