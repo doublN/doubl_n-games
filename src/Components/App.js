4805d53d53709181618c717c0c2076b884203658
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-16 10:53:44 +0100

    Added a user context to track the user logged in (currently hard coded)

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import '../App.css';
+import {UserContext} from '../Contexts/UserContext'
 import Header from './Header.js'
 import Navbar from './Navbar'
 import ReviewPage from './ReviewPage';
@@ -8,16 +9,19 @@ import Reviews from './Reviews'
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState("");
 
   return (
     <>
-      <Header setIsLoading={setIsLoading}/>
-      <Navbar setIsLoading={setIsLoading}/>
-      <Routes>
-        <Route path="/" element={<Reviews isLoading={isLoading} setIsLoading={setIsLoading}/>} />
-        <Route path="/:category" element={<Reviews isLoading={isLoading} setIsLoading={setIsLoading}/>} />
-        <Route path="/review/:review_id" element={<ReviewPage />} />
-      </Routes>
+      <UserContext.Provider value={{user, setUser}}>
+        <Header setIsLoading={setIsLoading}/>
+        <Navbar setIsLoading={setIsLoading}/>
+        <Routes>
+          <Route path="/" element={<Reviews isLoading={isLoading} setIsLoading={setIsLoading}/>} />
+          <Route path="/:category" element={<Reviews isLoading={isLoading} setIsLoading={setIsLoading}/>} />
+          <Route path="/review/:review_id" element={<ReviewPage />} />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
--- src/Components/PostComment.js
+++ src/Components/PostComment.js
@@ -1,22 +1,24 @@
-import {useState} from 'react'
+import {useState, useContext} from 'react'
 import {useParams} from 'react-router-dom'
 import { postComment } from '../Utils/api';
+import {UserContext} from '../Contexts/UserContext'
 
 export default function PostComment({setComments}) {
   const [comment, setComment] = useState("")
   const [buttonDisabled, setButtonDisabled] = useState(false)
+  const {user} = useContext(UserContext)
 
   const {review_id} = useParams();
   function handleSubmit(event){
     event.preventDefault();
     setButtonDisabled(true);
-    postComment(review_id, 'tickle122', comment).then((comment) =>{
+    postComment(review_id, user, comment).then((comment) =>{
       setComments((currentComments) =>{
         setButtonDisabled(false);
         return [comment, ...currentComments]
       })
     }).catch((err) =>{
-      alert("Error posting comment, please try again");
+      alert("Error posting comment, please try again (are you logged in?)");
       setButtonDisabled(false);
     });
     setComment("");
