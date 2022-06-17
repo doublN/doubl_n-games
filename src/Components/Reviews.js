fcb90fc844dd4749e769d9abde363d3cffaa7f67
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-06-17 10:26:18 +0100

    Set up SortBar and enabled sorting depending on which option is clicked

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,10 +1,13 @@
 import {useState, useEffect} from 'react'
 import {useParams, Link} from 'react-router-dom'
 import ReviewCard from './ReviewCard.js'
+import SortBar from './SortBar.js'
 import {getReviews} from '../Utils/api'
 
 export default function Reviews({isLoading, setIsLoading}) {
     const [reviews, setReviews] = useState([]);
+    const [sortBy, setSortBy] = useState(null);
+    const [orderBy, setOrderBy] = useState(null);
 
     const {category} = useParams();
 
@@ -12,22 +15,27 @@ export default function Reviews({isLoading, setIsLoading}) {
     getReviews(category).then((reviews) =>{
         setReviews(reviews);
         setIsLoading(false);
+
+        return () => {setSortBy(null); setOrderBy(null)}
     })
-    }, [category])
+    }, [category, orderBy, sortBy])
 
     if(isLoading){
         return <p className='loadingText'>... Loading!</p>
     }
 
     return (
-        <ul className="reviewsList">
-            {reviews.map((review) =>{
-                return(
-                    <Link to={`/review/${review.review_id}`} key={review.review_id} className='linkStyle'>
-                        <ReviewCard review={review} />
-                    </Link>
-                )
-            })}
-        </ul>
+        <>
+            <SortBar setSortBy={setSortBy} sortBy={sortBy} setOrderBy={setOrderBy} orderBy={orderBy}/>
+            <ul className="reviewsList">
+                {reviews.map((review) =>{
+                    return(
+                        <Link to={`/review/${review.review_id}`} key={review.review_id} className='linkStyle'>
+                            <ReviewCard review={review} />
+                        </Link>
+                    )
+                })}
+            </ul>
+        </>
     )
 }
